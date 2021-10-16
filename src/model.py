b385ce19dea6fc3483b13689c4aacfeaf4673102
Max Pershin <mepershin@gmail.com>
2021-10-16 12:36:19 +0000

    config

--- src/model.py
+++ src/model.py
@@ -5,14 +5,11 @@ from torchtyping import TensorType
 
 class ASRModel(nn.Module):
 
-    def __init__(self):
+    def __init__(self, n_classes: int,
+                       rnn_num_layers: int,
+                       rnn_hidden_size: int,
+                       rnn_input_size: int):
         super().__init__()
-
-        self.rnn_hidden_size = 64
-        self.rnn_input_size = 352
-        self.rnn_num_layers = 2
-
-        self.n_classes = 29
 
         self.conv_filter = nn.Sequential(
                 nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(8, 32),
@@ -26,15 +23,15 @@ class ASRModel(nn.Module):
             )
 
         self.rnn_filter = nn.GRU(
-                input_size=self.rnn_input_size,
-                hidden_size=self.rnn_hidden_size,
-                num_layers=self.rnn_num_layers,
+                input_size=rnn_input_size,
+                hidden_size=rnn_hidden_size,
+                num_layers=rnn_num_layers,
                 batch_first=True,
                 bidirectional=False,
             )
 
         self.head = nn.Sequential(
-                nn.Linear(in_features=self.rnn_hidden_size, out_features=self.n_classes, bias=True),
+                nn.Linear(in_features=rnn_hidden_size, out_features=n_classes, bias=True),
             )
 
     def forward(self, X: TensorType['batch', 'n_mels', 'time']):
--- train.py
+++ train.py
@@ -1,24 +1,107 @@
 import torch
-import torchaudio
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.cli import LightningCLI
+
 from src.dataset import LibriSpeechDataset
 from src.model import ASRModel
 
-df = LibriSpeechDataset('data/', 'train-clean-360')
-model = ASRModel()
-criterion = torch.nn.CTCLoss()
+from typing import (
+    Any,
+    Dict,
+    List,
+)
 
-batch = torch.unsqueeze(df[1][0], dim=0)
+#  df = LibriSpeechDataset('data/', 'train-clean-360')
+#  model = ASRModel()
+#  criterion = torch.nn.CTCLoss()
 
-tr = torchaudio.transforms.MelSpectrogram(
-        sample_rate=16_000,
-        n_fft=1024,
-        win_length=1024,
-        hop_length=256,
-        n_mels=64,
-    )
+#  batch = torch.unsqueeze(df[1][0], dim=0)
 
-batch = tr(batch)
-out = model(batch)
+#  tr = torchaudio.transforms.MelSpectrogram(
+#          sample_rate=16000,
+#          n_fft=1024,
+#          win_length=1024,
+#          hop_length=256,
+#          n_mels=64,
+#      )
 
-out = torch.transpose(out, 0, 1)
-print(out.shape)
+#  batch = tr(batch)
+#  out = model(batch)
+
+#  out = torch.transpose(out, 0, 1)
+#  loss = criterion()
+
+class LibrispeechDataModule(pl.LightningDataModule):
+
+    def __init__(self, train_root: str,
+                       train_df_url: str,
+                       train_batch_size: int,
+                       train_num_workers: int,
+                       val_root: str,
+                       val_df_url: str,
+                       val_batch_size: int,
+                       val_num_workers: int):
+        super().__init__()
+        self.train_dataset_kwargs = {
+                'root': train_root,
+                'url': train_df_url,
+            }
+        self.train_dataloader_kwargs = {
+                'batch_size': train_batch_size,
+                'num_workers': train_num_workers,
+            }
+        self.val_dataset_kwargs = {
+                'root': val_root,
+                'url': val_df_url,
+            }
+        self.val_dataloader_kwargs = {
+                'batch_size': val_batch_size,
+                'num_workers': val_num_workers,
+            }
+
+    def train_dataloader(self) -> DataLoader:
+        dataset = LibriSpeechDataset(**self.train_dataset_kwargs)
+        return DataLoader(dataset, **self.train_dataloader_kwargs)
+
+    def val_dataloader(self) -> DataLoader:
+        dataset = LibriSpeechDataset(**self.val_dataset_kwargs)
+        return DataLoader(dataset, **self.val_dataloader_kwargs)
+
+
+class ASRLightningModule(pl.LightningModule):
+
+    def __init__(self, model: ASRModel,
+                       criterion: nn.Module,
+                       optimizer_lr: float):
+        super().__init__()
+        self.model = model
+        self.criterion = criterion
+        self.optimizer_lr = optimizer_lr
+
+    def configure_optimizers(self) -> Dict[str, Any]:
+        optim = torch.optim.Adam(self.model.parameters(), lr=self.optimizer_lr)
+        return {
+                'optimizer': optim,
+            }
+
+    def training_step(self, batch, batch_idx) -> Dict[str, Any]:
+        pass
+
+    def training_epoch_end(self, outputs: List[Dict[str, Any]]) -> None:
+        pass
+
+    def validation_step(self, batch, batch_idx) -> Dict[str, Any]:
+        pass
+
+    def validation_epoch_end(self, outputs: List[Dict[str, Any]]) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    LightningCLI(
+            ASRLightningModule,
+            LibrispeechDataModule,
+        )

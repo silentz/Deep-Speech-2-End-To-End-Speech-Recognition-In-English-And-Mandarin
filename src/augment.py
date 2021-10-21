38dc862e0b7dd39c45a1fdae379b2c0ab0b455fc
Max Pershin <mepershin@gmail.com>
2021-10-21 10:45:45 +0000

    augmentations

---
+++ src/augment.py
@@ -0,0 +1,14 @@
+from audiomentations import (
+    Compose,
+    AddGaussianNoise,
+    TimeStretch,
+    PolarityInversion,
+    PitchShift,
+)
+
+train_transforms = Compose([
+    AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
+    TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5),
+    PolarityInversion(p=0.5),
+    PitchShift(min_semitones=-4, max_semitones=4, p=0.5),
+])

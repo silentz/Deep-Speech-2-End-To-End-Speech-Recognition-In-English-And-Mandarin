a172743b2cd2dc330f4b053ac87c8ad330ce7d6a
Max Pershin <mepershin@gmail.com>
2021-10-22 09:38:55 +0000

    transfer spectrograms to dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
+import math
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 from torchaudio.datasets import LIBRISPEECH, LJSPEECH
@@ -6,13 +8,18 @@ from typing import Any, Callable, Dict
 from .text import encode
 
 
+CLAMP_MIN_THRESHOLD = 1e-5
+
+
 class LibrispeechDataset(Dataset):
 
     def __init__(self, root: str,
                        url: str,
-                       transforms: Callable):
+                       transforms: Callable,
+                       spectrogram: nn.Module):
         self.dataset = LIBRISPEECH(root=root, url=url, download=True)
         self.transforms = transforms
+        self.spectrogram = spectrogram
 
     def __len__(self):
         return len(self.dataset)
@@ -22,20 +29,29 @@ class LibrispeechDataset(Dataset):
         wave = torch.mean(wave, dim=0)
         wave = self.transforms(wave.numpy(), sample_rate=sample_rate)
         wave = torch.from_numpy(wave)
+
+        spectrogram = self.spectrogram(wave)
+        spectrogram = torch.transpose(spectrogram, 0, 1)
+        spectrogram = spectrogram.clamp(min=CLAMP_MIN_THRESHOLD).log()
+
         text = torch.LongTensor(encode(text))
         return {
                 'wave': wave,
                 'wave_len': len(wave),
                 'text': text,
                 'text_len': len(text),
+                'spectrogram': spectrogram,
             }
 
 
 class LJSpeechDataset(Dataset):
 
-    def __init__(self, root: str, transforms: Callable):
+    def __init__(self, root: str,
+                       transforms: Callable,
+                       spectrogram: nn.Module):
         self.dataset = LJSPEECH(root=root, download=True)
         self.transforms = transforms
+        self.spectrogram = spectrogram
 
     def __len__(self):
         return len(self.dataset)
@@ -45,12 +61,18 @@ class LJSpeechDataset(Dataset):
         wave = torch.mean(wave, dim=0)
         wave = self.transforms(wave.numpy(), sample_rate=sample_rate)
         wave = torch.from_numpy(wave)
+
+        spectrogram = self.spectrogram(wave)
+        spectrogram = torch.transpose(spectrogram, 0, 1)
+        spectrogram = spectrogram.clamp(min=CLAMP_MIN_THRESHOLD).log()
+
         text = torch.LongTensor(encode(text))
         return {
                 'wave': wave,
                 'wave_len': len(wave),
                 'text': text,
                 'text_len': len(text),
+                'spectrogram': spectrogram,
             }
 
 
@@ -74,17 +96,21 @@ class PartialDataset(Dataset):
 def collate_fn(values: list) -> Dict[str, Any]:
     waves = [x['wave'] for x in values]
     texts = [x['text'] for x in values]
+    spectrograms = [x['spectrogram'] for x in values]
 
     waves_len = [x['wave_len'] for x in values]
     texts_len = [x['text_len'] for x in values]
 
     waves = pad_sequence(waves, batch_first=True, padding_value=0)
     texts = pad_sequence(texts, batch_first=True, padding_value=0)
+    spectrograms = pad_sequence(spectrograms,
+                    batch_first=True, padding_value=math.log(CLAMP_MIN_THRESHOLD))
 
     return {
             'waves': waves,
             'waves_len': torch.LongTensor(waves_len),
             'texts': texts,
             'texts_len': torch.LongTensor(texts_len),
+            'spectrograms': spectrograms,
         }
 
